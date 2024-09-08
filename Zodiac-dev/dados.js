9abbb330074dbc05c17948d028e1061388ede970
Kamado328 <pedruguilherme328@gmail.com>
2024-09-08 20:16:28 -0300

    Add files via upload

---
+++ Zodiac-dev/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Jogos na Educação: Uma Abordagem Divertida e Eficaz",
+        descricao: "Ao proporcionar um ambiente lúdico e interativo, os jogos estimulam a curiosidade, a criatividade e o desenvolvimento de diversas habilidades cognitivas e socioemocionais.",
+        link: "https://www.harvard.edu/in-focus/games/",
+        tags: "educação educacao games jogos"
+    },
+    {
+        titulo: "Gamificação na Educação: Um Novo Rumo para a Aprendizagem",
+        descricao: "Descubra como a gamificação está revolucionando a forma como aprendemos. Através de elementos de jogos, como pontos, níveis e desafios, os alunos se tornam mais engajados e motivados a alcançar seus objetivos.",
+        link: "https://www.educamaisbrasil.com.br/educacao/noticias/gamificacao-o-que-e-e-como-utilizar-em-sala-de-aula?gad_source=1&gclid=Cj0KCQjw0Oq2BhCCARIsAA5hubUbZeYh1GwQ-v2yh2Xhk5TGuUOpYEghVZMJUdU7hpfuSYHiWXLR7GMaAtjbEALw_wcB",
+        tags: "educação educacao gamificacao gamificação"
+    },
+    {
+        titulo: "Os Benefícios da Gamificação para o Desenvolvimento de Habilidades",
+        descricao: "A gamificação não é apenas divertida, mas também eficaz para desenvolver habilidades como criatividade, resolução de problemas e trabalho em equipe. Neste artigo, exploramos os benefícios da gamificação para o aprendizado.",
+        link: "https://genially.com/pt-br/",
+        tags: "habilidades criatividade beneficios"
+    }
+];
